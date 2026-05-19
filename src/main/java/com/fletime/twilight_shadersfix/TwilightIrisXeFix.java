8654c4f0dd2d6d0c60760744ab6f3424f43bdcc9
DoYmL8c <HSeEnV5@proton.me>
2026-05-19 16:24:44 +0800

    初始提交

---
+++ src/main/java/com/fletime/twilight_shadersfix/TwilightIrisXeFix.java
@@ -0,0 +1,11 @@
+package com.fletime.twilight_shadersfix;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TwilightIrisXeFix.MOD_ID)
+public class TwilightIrisXeFix {
+    public static final String MOD_ID = "twilight_iris_xe_fix";
+
+    public TwilightIrisXeFix() {
+    }
+}
